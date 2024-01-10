7287cd5972b809bd56712d69e42ca4ae7dd5341d
Mario Costa <mario.costa.13@gmail.com>
2024-01-10 11:23:17 +0000

    Feature: Create and Update Movie

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 )
 
 type Movie struct {
@@ -53,6 +55,36 @@ func getMovie(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(100000000))
+	movies = append(movies, movie)
+
+	json.NewEncoder(w).Encode(movie)
+}
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			break
+		}
+	}
+
+	json.NewEncoder(w).Encode(movie)
+}
+
 func main() {
 	r := mux.NewRouter()
 
@@ -62,8 +94,8 @@ func main() {
 	// Route Handlers / Endpoints
 	r.HandleFunc("/api/movies", getMovies).Methods("GET")
 	r.HandleFunc("/api/movies/{id}", getMovie).Methods("GET")
-	//r.HandleFunc("/api/movies", createMovie).Methods("POST")
-	//r.HandleFunc("/api/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/api/movies", createMovie).Methods("POST")
+	r.HandleFunc("/api/movies/{id}", updateMovie).Methods("PUT")
 	r.HandleFunc("/api/movies/{id}", deleteMovie).Methods("DELETE")
 
 	fmt.Printf("Starting server at port 8000\n")
